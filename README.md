ca778328f83384a733a168969a4ac7d35a2c1746
Lukas Milašauskas <lukas.milasauskas@inventi.lt>
2021-05-12 20:10:30 +0300

    feat: add readme, docker compose disable feature flags polling, configure actuator liveness and readiness probes for kubernetes cluster

---
+++ README.md
@@ -0,0 +1,5 @@
+# Continuous Delivery pipeline app
+
+Project created to implement CD pipeline for Java applications example with achieved highest maturity level from technical perspective.
+
+Project created for Vilnius University bachelor degree paper. 
--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -8,9 +8,6 @@ if ! helm repo list|grep $REPO_URL > /dev/null; then
     helm repo add inventi $REPO_URL
 fi
 
-env_override_file=helm-values.env.${ENVIRONMENT}.yaml
-env_override_option="$([ -f $env_override_file ] && echo "-f $env_override_file")"
-
 helm upgrade -i $RELEASE inventi/$CHART --version $CHART_VER \
     -f helm-values.yaml \
     --set image.tag=$VERSION \
