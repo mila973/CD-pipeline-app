37dae90c0477007ece8c04e6e68ea6c01a4f5f71
Lukas Milašauskas <lukas.milasauskas@inventi.lt>
2021-05-12 22:54:27 +0300

    test: add factorial endpoint tests

--- src/test/java/com/mif/pipeline/controller/v1/TaskControllerTest.java
+++ src/test/java/com/mif/pipeline/controller/v1/TaskControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
@@ -49,11 +51,23 @@ class TaskControllerTest {
 
     @Test
     void returns_bar_when_bar_called() {
-        // When
+        // Given, When
         String result = controller.getBar();
 
         // Then
         assertEquals("BAR", result);
+    }
+
+    @Test
+    void returns_24_when_factorial_called_with_4() {
+        // Given
+        BigInteger expectedResult = BigInteger.valueOf(24);
+        given(factorialService.calculateFactorial(4))
+            .willReturn(expectedResult);
+        // When
+        BigInteger result = controller.getFactorial(4);
+        // Then
+        assertEquals(expectedResult, result);
     }
 
 }
